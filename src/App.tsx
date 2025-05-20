7090d21aa079563e3135c3c9d96e148d4a05b737
JustNobleMe <isaacodey01@gmail.com>
2025-05-20 02:49:32 -0700

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import { Analytics } from "@vercel/analytics/next";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -23,6 +23,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+    <Analytics />
     <TooltipProvider>
       <Toaster />
       <Sonner />
